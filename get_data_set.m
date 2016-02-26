70a7c275d5b93746f6ccb996d11492d18ffce596
Mario Gianni <gianni@dis.uniroma1.it>
2016-02-26 09:12:58 +0100

    added mat files

---
+++ get_data_set.m
@@ -0,0 +1,49 @@
+clear all
+close all
+
+filename1 = '20160225171559_seqA';
+filename2 = '20160225171559_seqB';
+filename3 = '20160225171559_seqc';
+
+d1 = csvread(['csvdata/' filename1 '.csv']);
+d2 = csvread(['csvdata/' filename2 '.csv']);
+d3 = csvread(['csvdata/' filename3 '.csv']);
+
+D = d1;
+
+%labels{68}='state->x';
+%labels{69}='state->y';
+%labels{70}='state->yaw';
+%labels{71}='state->xD';
+%labels{72}='state->yD';
+%labels{73}='state->yawD'
+
+i_robot_pos_deriv = [71,72,73];
+robot_pos_deriv = D(:,i_robot_pos_deriv);
+[n,m] = size(robot_pos_deriv);
+robot_deriv = zeros((n-1),2*m);
+robot_deriv(:,1:m) = robot_pos_deriv(1:(n-1),:);
+robot_deriv(:,(m+1):end) = robot_pos_deriv(2:n,:);
+
+%labels{ 6}='tf_base_link_world.getRotation().getX()';  
+%labels{ 7}='tf_base_link_world.getRotation().getY()';  
+%labels{ 8}='tf_base_link_world.getRotation().getZ()';  
+%labels{ 9}='tf_base_link_world.getRotation().getW()';
+
+i_robot_quat = [6,7,8,9];
+robot_quat = D(:,i_robot_quat);
+[n,m] = size(robot_quat);
+robot_euler = zeros(n,3);
+for i = 1 : n
+    robot_euler(i,:) = quat2angle(robot_quat(i,:));
+end
+robot_yaw = robot_euler(:,1);
+
+state = [robot_deriv,robot_yaw(1:(end-1))];
+
+%labels{11}='tracks_cmd->left';
+%labels{12}='tracks_cmd->right';
+i_vel = [11,12];
+cmd_vel = D(1:(end-1),i_vel);
+
+save(['dataset_g/' filename1 '.mat'],'state','cmd_vel')
---
+++ main.m
@@ -0,0 +1,50 @@
+clear all
+close all
+
+filename1 = '20160225171559_seqA';
+filename2 = '20160225171559_seqB';
+filename3 = '20160225171559_seqc';
+
+load(['dataset_g/' filename1 '.mat']);
+
+addpath(genpath('/Users/Mario/github/machine_learning/multi_task_learning_gp'));
+addpath(genpath('/Users/Mario/github/machine_learning/gaussian_processes'));
+
+
+%% 1. Generating samples
+p = 0.8;
+[ xtrain, ytrain,x,Y, M, D, nx, ind_kf_train, ind_kx_train ] = split_data_set(state,cmd_vel,p);
+
+%% 2. Assigns cell data for learning and prediction
+covfunc_x = {'covSEard'};
+%covfunc_x = {'covSum', {'covSEard','covNoise'}};
+irank = M; % rank for Kf (1, ... M). irank=M -> Full rank
+data  = {covfunc_x, xtrain, ytrain, M, irank, nx, ind_kf_train, ind_kx_train};
+
+%% 3. Hyper-parameter learning
+[logtheta_all,deriv_range] = init_mtgp_default(xtrain, covfunc_x, M, irank);
+disp('Learning');
+[logtheta_all,nl] = learn_mtgp(logtheta_all, deriv_range, data);
+
+plot(nl,'linewidt',3); 
+title('Negative Marginal log-likelihood');
+
+%% 4. Making predictions at all points on all tasks
+disp('Prediction');
+[ Ypred, Vpred ] = predict_mtgp_all_tasks(logtheta_all, data, x );
+disp('Finished');
+
+%% 5. Component-wise Euclidean Distance evaluation
+[n,m] = size(Y);
+error = zeros(n,1);
+for i = 1 : M
+    error_c = (Y(:,i) - Ypred(:,i)).^2;
+    error = error + error_c; 
+end
+error_final = sqrt(error);
+plot(error_final,'linewidt',3); 
+title('Component-wise Euclidean Distance');
+
+mse = norm((Y - Ypred).^2);
+fprintf('Froebenius Norm: %15.5f\n',mse);
+
